3f315008ea63b0f63e22a222b94e1f9cc8d40376
Johan von Hacht <johvh@kth.se>
2019-01-17 19:08:37 +0100

    changed default url/app

--- aktiearbitrage/urls.py
+++ aktiearbitrage/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/', include('mainpage.urls'))
+    url(r'', include('mainpage.urls'))
 ]
