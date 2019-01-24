b9f7a398419432abff10e375842b040adef6a98b
Johan von Hacht <johvh@kth.se>
2019-01-24 14:41:09 +0100

    Dynamic list with ajax get

---
+++ mainpage/static/js/functions.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    getListItems("", "");
+});
+
+/**
+ * Function iterate and append list item.
+ */
+function renderListItems(stockList) {
+    var container = $("#list-group");
+    for (var key in stockList) {
+        // check if the property/key is defined in the object itself, not in parent
+        if (stockList.hasOwnProperty(key)) {           
+            var itemdiv = "<div class=\"list-group-item align-items-center\"> \
+                    <p class=\"name\">Hejsan</p> \
+                    </div>"
+            container.append(itemdiv);
+        }
+    }
+}
+
+/**
+ * Retrieve stocks from database, based on query and sorting method.
+ */
+function getListItems(query, sort) {
+    $.ajax({
+        url: 'ajax/get_stock_list',
+        datatype: 'json',
+        data : {
+            'query': query,
+            'sort': sort
+        },
+        type: 'GET',
+        success: function(data) {
+            renderListItems(data);
+        }
+    });
+}
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from mainpage.models import Stock, Stock_Info, Stock_Price
+import json
+from django.http import JsonResponse
 
 # Main view
 def home(request):
     # query database
     # 
-    stocks = Stock.objects.all()
-    #print(stocks[0])
-    #stock_info = Stock_Info.objects.get(stock=stocks[0])
-    #print(stocks)
-    return render(request, 'mainpage/stock_list.html', {'stocks':stocks})
+    return render(request, 'mainpage/stock_list.html')
+
+def get_stock_list(request):
+    data = {
+        'name': 'Vitor',
+        'location': 'Finland',
+        'is_active': True,
+        'count': 28
+    }
+    return JsonResponse(data)
 
 def send(request):
     p = Stock(name='Handelsbanken')
