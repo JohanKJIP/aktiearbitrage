ef80d2d4ff43d5c462d67300177e126b851552ff
Johan von Hacht <johvh@kth.se>
2019-02-01 21:03:46 +0100

    Basic login/logout with django auth

--- aktiearbitrage/urls.py
+++ aktiearbitrage/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'', include('mainpage.urls'))
 ]
